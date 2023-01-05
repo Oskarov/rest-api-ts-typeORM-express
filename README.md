8fe214ee8cb8b05042c51a0445a9bf765019bf91
Di <elven.legacy@hotmail.com>
2023-01-05 16:28:17 +0300

    installing TypeORM, connecting with docker postgres

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# rest-api-ts-typeORM-express
-learning typeORM with ts, making another restfull api
+# REST api with TypeORM, TS, Express
+
+### REST api from Kris Foster lessons
+
+---
+
+Personal project of [Dmitriy Oskarov](#)
+
+---
+
+Technology stack:
+
+* Node JS
+* Express
+* TS
+* TypeORM
+* Postgre
+* Docker
+
+---
+
+### Attention
+
+This project is not a demonstration of optimal solutions and is aimed at studying specific things.
+
+Этот проект не является демонстрацией оптимальных решений и направлен на изучение конкретных технологий и подходов.
+
+---
+
+### Description
+
+...
+
+---
+
+### Installing
+
+1. clone
+2. npm install
+3. docker compose up -d
+4. npm run start
+
+---
+
+thanks [Vlad Minin](https://www.youtube.com/@KrisFoster1) for lessons and inspiration
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { Request, Response } from "express"
+import { AppDataSource } from "./data-source"
+import { Routes } from "./routes"
+import { User } from "./entity/User"
+
+AppDataSource.initialize().then(async () => {
+
+    // create express app
+    const app = express()
+    app.use(bodyParser.json())
+
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next)
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result)
+            }
+        })
+    })
+
+    // setup express app here
+    // ...
+
+    // start express server
+    app.listen(3000)
+
+}).catch(error => console.log(error))
