429905f0519623899cf61ea611b25e314033be12
Di <elven.legacy@hotmail.com>
2023-01-07 21:20:30 +0300

    handle errors on middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+import * as express        from "express"
+import * as bodyParser     from "body-parser"
+import {Request, Response} from "express"
+import {AppDataSource}     from "./data-source"
+import {Routes}            from "./routes"
+import {User}              from "./entity/User";
+
+const handleErrors = (err, req, res, next) => {
+    res.status(err.statusCode || 500).send({field: err.field, message: err.message});
+}
 
 AppDataSource.initialize().then(async () => {
 
@@ -13,21 +17,18 @@ AppDataSource.initialize().then(async () => {
 
     // register express routes from defined application routes
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
+        (app as any)[route.method](route.route, async (req: Request, res: Response, next: Function) => {
+            try {
+                const result = await (new (route.controller as any))[route.action](req, res, next)
+                res.json(result);
+            } catch (e) {
+                next(e);
             }
         })
     })
 
-    // setup express app here
-    // ...
+    app.use(handleErrors);
 
-    // start express server
     app.listen(3000)
 
 }).catch(error => console.log(error))
